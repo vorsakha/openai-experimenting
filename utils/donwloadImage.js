8e42e70994614b2ee90725f74e0e397e5c8d1b1c
Andrei Ferreira <andreitf.dev@gmail.com>
2022-11-20 10:14:03 -0300

    fix: silence eslint no unnused vars

--- utils/donwloadImage.js
+++ utils/donwloadImage.js
@@ -2,6 +2,7 @@ import request from "request";
 import fs from "fs";
 
 export default (url, path, callback) => {
+  // eslint-disable-next-line no-unused-vars
   request.head(url, (err, res, body) => {
     request(url).pipe(fs.createWriteStream(path)).on("close", callback);
   });
